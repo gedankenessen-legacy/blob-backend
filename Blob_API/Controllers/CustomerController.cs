15068e9ea3176402bffcfed019c2821e6ed9ecb9
mschwer1 <morit@141.79.52.171>
2020-05-12 19:06:32 +0200

    Added GetAllCustomers and GetCustomer operations.

--- Blob_API/Controllers/CustomerController.cs
+++ Blob_API/Controllers/CustomerController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Blob_API.Model;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,5 +12,27 @@ namespace Blob_API.Controllers
     [ApiController]
     public class CustomerController : ControllerBase
     {
+        private readonly BlobContext _context;
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Customer>> GetAllCustomers()
+        {
+            return _context.Customer.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Order> GetCustomer(int id)
+        {
+            var res = _context.Customer.Find(id);
+
+            if (res == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(res);
+        }
+
+
     }
 }
